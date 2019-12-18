10cdb326cb45aea18161aca7de087076b3f30451
nicolasgere <nicolas.gerelm@gmail.com>
2019-12-17 21:45:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(request, response) {
     console.log(process.env)
+    
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello world in production, yes! " + process.env.TEST + "thib");
 

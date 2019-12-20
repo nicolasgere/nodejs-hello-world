39c0781439e588c51b89b672a6520ba6185ff16d
nicolasgere <nicolas.gerelm@gmail.com>
2019-12-20 17:48:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var server = http.createServer(function(request, response) {
     console.log(process.env)
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello world in production, yes! " + process.env.TEST + "thib");  
+    
 });
 
 var port = 80;

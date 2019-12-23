bbb5f1c7f1fa449e8acf6d8b6b9ad086482d4af9
nicolasgere <nicolas.gerelm@gmail.com>
2019-12-23 15:41:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var http = require('http');
 
+function fibonacci(n) {
+   return n < 1 ? 0
+        : n <= 2 ? 1
+        : fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 var server = http.createServer(function(request, response) {
     console.log(process.env)
+    fibonacci(5)
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello world in production, yes! " + process.env.TEST + "thib");  
 });

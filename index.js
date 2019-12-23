ea19260fabbfa032c1934beee2c93be2e5b47a63
nicolasgere <nicolas.gerelm@gmail.com>
2019-12-23 16:33:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function fibonacci(n) {
    return n < 1 ? 0
         : n <= 2 ? 1
         : fibonacci(n - 1) + fibonacci(n - 2);
+   
 }
 
 var server = http.createServer(function(request, response) {

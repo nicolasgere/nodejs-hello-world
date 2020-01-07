85a4de659dd6f9142c8a8a116894e65d62814984
nicolasgere <nicolas.gerelm@gmail.com>
2020-01-06 22:46:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function fibonacci(n) {
 
 var server = http.createServer(function(request, response) {
     fibonacci(30)
-   
     console.log(process.env.TEST)
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello world in production, yes! " + process.env.TEST + "thib");  
